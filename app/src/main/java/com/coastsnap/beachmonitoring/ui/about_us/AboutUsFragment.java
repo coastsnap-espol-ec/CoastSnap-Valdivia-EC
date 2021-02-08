366937651b4ba19b17765a3534f538c0e6737222
Javier <jasearce@espol.edu.ec>
2021-02-08 14:12:25 -0500

    modificacion de about us page y depuracion de home page

--- app/src/main/java/com/coastsnap/beachmonitoring/ui/about_us/AboutUsFragment.java
+++ app/src/main/java/com/coastsnap/beachmonitoring/ui/about_us/AboutUsFragment.java
@@ -1,17 +1,27 @@
 package com.coastsnap.beachmonitoring.ui.about_us;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.coastsnap.beachmonitoring.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class AboutUsFragment extends Fragment {
 
     private AboutUsViewModel aboutUsViewModel;
+    private String mDescription[] = {"Facebook Description", "Instagram Description", "Twitter Description", "Youtube Description", "Github Description"};
+    private int images[] = {R.drawable.facebook_logo, R.drawable.instagram_logo, R.drawable.twitter_logo, R.drawable.youtube_logo, R.drawable.github_logo};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -19,4 +29,5 @@ public class AboutUsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_about_us, container, false);
         return root;
     }
+
 }
