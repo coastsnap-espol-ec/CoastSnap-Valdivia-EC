2404b5646d005583e677b81b7d9b86ee3d2e7689
Javier <jasearce@espol.edu.ec>
2021-02-08 22:02:52 -0500

    se agrega inicio en camara luego de ingreso por primera vez

--- app/src/main/java/com/coastsnap/beachmonitoring/SplashScreen.java
+++ app/src/main/java/com/coastsnap/beachmonitoring/SplashScreen.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 public class SplashScreen extends AppCompatActivity {
 
     public static long SPLASH_TIME_OUT = 5000;   //4 segundos
+    public int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +19,18 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                if (i == 0) {
+                    Intent toDashboardActivity = new Intent(SplashScreen.this, DashboardActivity.class);
+                    startActivity(toDashboardActivity);
+                    i++;
+                } else {
+                    Intent toCamera = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(toCamera);
+                    finish();
+                }
                 //Intent toMainActivity = new Intent(SplashScreen.this, MainActivity.class);
-                Intent toDashboardActivity = new Intent(SplashScreen.this, DashboardActivity.class);
-                startActivity(toDashboardActivity);
-                finish();
+
+                //finish();
             }
         }, SPLASH_TIME_OUT);
     }
